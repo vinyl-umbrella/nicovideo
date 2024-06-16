f1a0a0229c2360f3a517e36e0b99cbc99458cb4b
vinyl-umbrella <61788251+vinyl-umbrella@users.noreply.github.com>
2024-06-17 06:32:01 +0900

    add cors header

--- backend/internal/infra/router/router.go
+++ backend/internal/infra/router/router.go
@@ -4,13 +4,22 @@ import (
 	"nicovideo-api/internal/interface/handler"
 	"nicovideo-api/internal/interface/repo"
 	"nicovideo-api/internal/usecase"
+	"time"
 
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 	"gorm.io/gorm"
 )
 
 func SetupRouter(db *gorm.DB) *gin.Engine {
 	r := gin.Default()
+
+	// CORS
+	r.Use(cors.New(cors.Config{
+		AllowAllOrigins: true,
+		AllowMethods:    []string{"GET", "OPTIONS"},
+		MaxAge:          12 * time.Hour,
+	}))
 
 	videoRepo := repo.NewVideoRepository(db)
 	videoUsecase := usecase.NewVideoUsecase(videoRepo)
